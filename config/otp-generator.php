a415fc54811926dccfa83a101438a6cb65643215
Morris Chavez <hello@morcen.com>
2022-03-14 14:36:52 +0800

    initial commit

---
+++ config/otp-generator.php
@@ -0,0 +1,5 @@
+<?php
+// config for Morcen/LaravelOtpGenerator
+return [
+
+];
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Morcen\LaravelOtpGenerator\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
